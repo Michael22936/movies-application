bfa7e999de9bb698b502b4e52807f6542e0efcf6
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-06-29 16:58:40 -0500

    got js to add a movie to the json

--- src/index.js
+++ src/index.js
@@ -105,29 +105,9 @@ getMovies().then((movies) => {
 
 
 $('#myButton').on("click", function() {
-  console.log('click');
-  var modal = document.getElementById("myModal1");
+  $('#myModal1').modal('toggle');
 
-// Get the button that opens the modal
-  var btn = document.getElementById("myButton");
-
-// Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  };
-
-// When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+  $('#add-movie-btn').on('click',function () {
     addMovie();
-  };
-
-// When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+  })
 });
